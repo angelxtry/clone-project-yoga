b766f1d4c8a212a09cd929540e6134771edbcc8a
LeeSuHo <angelxtry@gmail.com>
2020-02-27 16:15:13 +0900

    Create App

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello');
+import { Options } from 'graphql-yoga';
+import app from './app';
+
+const PORT: string | number = process.env.PORT || 4000;
+const PLAYGROUND: string = '/playground';
+const GRAPHQL_ENDPOINT: string = '/graphql';
+
+const appOption: Options = {
+  port: PORT,
+  playground: PLAYGROUND,
+  endpoint: GRAPHQL_ENDPOINT,
+};
+
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
+
+app.start(appOption, handleAppStart);
