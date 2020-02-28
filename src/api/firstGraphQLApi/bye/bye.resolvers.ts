4241975b2c0d64561c774e132ab4ff1e360fd0dd
LeeSuHo <angelxtry@gmail.com>
2020-02-28 14:18:51 +0900

    Delete graphql test files

--- src/api/firstGraphQLApi/bye/bye.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-  Query: {
-    sayBye: () => 'Hello',
-  },
-};
-
-export default resolvers;
--- src/types/graphql.d.ts
+++ src/types/graphql.d.ts
@@ -1,19 +1,8 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): Greeting!\n  user: User\n}\n\ntype Greeting {\n  text: String!\n  error: Boolean!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int!\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
+export const typeDefs = ["type User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int!\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  sayBye: string;
-  sayHello: Greeting;
   user: User | null;
-}
-
-export interface SayHelloQueryArgs {
-  name: string;
-}
-
-export interface Greeting {
-  text: string;
-  error: boolean;
 }
 
 export interface User {
