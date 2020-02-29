51c2b2dbfbe71517702c3a35171eb847f6de03d9
LeeSuHo <angelxtry@gmail.com>
2020-02-29 16:40:53 +0900

    Add CompletePhoneVerification

---
+++ src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
@@ -0,0 +1,64 @@
+import User from '../../../entities/User';
+import Verification from '../../../entities/Verification';
+import { Resolvers } from '../../../types/resolvers';
+import {
+  CompletePhoneVerificationMutationArgs,
+  CompletePhoneVerificationResponse,
+} from '../../../types/graphql';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    CompletePhoneVerification: async (
+      _,
+      args: CompletePhoneVerificationMutationArgs,
+    ): Promise<CompletePhoneVerificationResponse> => {
+      const { phoneNumber, key } = args;
+      try {
+        const verification = await Verification.findOne({
+          payload: phoneNumber,
+          key,
+        });
+        if (!verification) {
+          return {
+            ok: false,
+            error: 'Verification key not valid',
+            token: null,
+          };
+        }
+        verification.verified = true;
+        verification.save();
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+      try {
+        const user = await User.findOne({ phoneNumber });
+        if (user) {
+          user.verifiedPhoneNumber = true;
+          user.save();
+          return {
+            ok: true,
+            error: null,
+            token: 'token',
+          };
+        }
+        return {
+          ok: true,
+          error: null,
+          token: null,
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
