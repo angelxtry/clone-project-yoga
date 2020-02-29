8aa181d3ab517d6bb052547d860b2073809f39dd
LeeSuHo <angelxtry@gmail.com>
2020-02-29 17:47:21 +0900

    Add jwt token

--- src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
+++ src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
@@ -1,3 +1,4 @@
+import createJWT from '../../../utils/createJWT';
 import User from '../../../entities/User';
 import Verification from '../../../entities/Verification';
 import { Resolvers } from '../../../types/resolvers';
@@ -39,10 +40,11 @@ const resolvers: Resolvers = {
         if (user) {
           user.verifiedPhoneNumber = true;
           user.save();
+          const token = createJWT(user.id);
           return {
             ok: true,
             error: null,
-            token: 'token',
+            token,
           };
         }
         return {
